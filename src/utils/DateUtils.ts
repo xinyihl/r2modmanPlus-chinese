4f9f887d50e84bad67813d677f3aaf2c72689bc1
xinyihl <1012737146@qq.com>
2025-11-17 10:54:37 +0800

    Date 显示汉化

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -1,4 +1,13 @@
+const formatter = new Intl.DateTimeFormat('zh-CN', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+});
+
 export function valueToReadableDate(date: number | Date): string {
     const dateObject: Date = new Date(date);
-    return `${dateObject.toDateString()}, ${dateObject.toLocaleTimeString()}`
+    return formatter.format(dateObject)
 }
